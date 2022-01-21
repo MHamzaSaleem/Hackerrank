1ebc8328d3b6c14787e63f8c590b27af86412b8c
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-01-21 16:16:18 +0500

    Electronics Shop Problem
    A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a give budget. Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to buy both items, return .

---
+++ Electronics Shop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hackerrank_Solutions
+{
+    class Bill_Division
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getMoneySpent(new int[] { 3, 1 }, new int[] { 5, 2, 8}, 10));
+            Console.Read();
+        }
+        static int getMoneySpent(int[] keyboards, int[] drives, int b)
+        {
+            int temp = -1;
+            for (int i = 0; i < keyboards.Length; i++)
+            {
+                for (int j = 0; j < drives.Length; j++)
+                {
+                    if (b >= (keyboards[i] + drives[j]) && temp < (keyboards[i] + drives[j]))
+                        temp = keyboards[i] + drives[j];
+                }
+            }
+            return temp;
+        }
+
+    }
+}
