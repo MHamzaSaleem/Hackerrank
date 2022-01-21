c1c58f480640620c73e20288f333ddfab30a7a14
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-01-21 15:58:03 +0500

    Counting Valleys Problem
    An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
    
    A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
    A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
    Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.

---
+++ Counting Valley.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hackerrank_Solutions
+{
+    class Bill_Division
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(countingValleys(8, "UDDDUDUUDUD"));
+            Console.Read();
+        }
+        public static int countingValleys(int steps, string path)
+        {
+            int currPosition = 0;
+            int valley = 0;
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (currPosition == 0 && path[i] == 'D')
+                    valley++;
+                currPosition += (path[i] == 'D') ? -1 : 1;
+            }
+            return valley;
+        }
+
+    }
+}
