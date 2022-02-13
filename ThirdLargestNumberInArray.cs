191b9e0694be03ae0860692784a63675afe5bb21
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-02-13 23:03:46 +0500

    Find Third Largest Number Of Array
    How to find third largest integer in an array using only one loop?
    Input: 3 2 1 5 4

---
+++ ThirdLargestNumberInArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test
+{
+    class ThirdLargestNumberInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 2, 1, 5, 4 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sortArray(ref arr, arr.Length-i, 0);
+            }
+            Console.WriteLine(arr[arr.Length - 3]);
+            Console.ReadKey();
+        }
+
+        private static void sortArray(ref int[] arr, int noOfSortItems, int i)
+        {
+            if (noOfSortItems-1 == i)
+                return;
+            else
+            {
+                var temp = arr[i];
+                if(arr[i]>arr[i+1])
+                {
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+                i++;
+                sortArray(ref arr, noOfSortItems, i);
+            }
+
+        }
+    }
+}
