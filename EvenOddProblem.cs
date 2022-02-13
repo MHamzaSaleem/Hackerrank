14d16a6793e70e912797035e4172965e05a9e88f
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-02-13 23:05:11 +0500

    if product of array is even then return sum of array else return 0

---
+++ EvenOddProblem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test
+{
+    class EvenOddProblem
+    {
+        static void Main(string[] args)
+        {
+            //if product of array is even then return sum of array else return 0
+            int[] arr = { 1, 2, 3, 4 };
+            int temp = 0;
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+                if (i == 0)
+                    temp = arr[i];
+                else
+                    temp *= arr[i];
+            }
+            if (temp % 2 == 0)
+                Console.WriteLine(sum);
+            else
+                Console.WriteLine(0);
+            Console.ReadKey();
+
+        }
+    }
+}
