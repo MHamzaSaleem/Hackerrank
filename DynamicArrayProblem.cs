29c35b71d531a155daf77224d52b89199fbbe7ef
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-02-15 14:42:21 +0500

    Dynamic Array Problem
    Declare a 2-dimensional array, arr, of  empty arrays. All arrays are zero indexed.
    Declare an integer, lastAnswer, and initialize it to 0.
    
    There are 2 types of queries, pa given as an array of strings for you to parse:
    
    1. Query: 1 x y
       1. Let idx = ((x ⊕ lastAnswer) % n).
       2. Append the integer y to arr[idx].
    2. Query: 2 x y
       1. Let idx = ((x ⊕ lastAnswer) % n).
       2. Assign the value arr[idx][y%size(arr[idx])] to lastAnswer.
       3. Store the new value of lastAnswer to an answers array.
    
    Note: ⊕ is the bitwise XOR operation, which corresponds to the ^ operator in most languages. Learn more about it on Wikipedia.% is the modulo operator.
    Finally, size(arr[idx]) is the number of elements in arr[idx]
    
    Function Description
    Complete the dynamicArray function below.
    dynamicArray has the following parameters:
    - int n: the number of empty arrays to initialize in arr
    - string queries[q]: query strings that contain 3 space-separated integers
    
    Returns
    int[]: the results of each type 2 query in the order they are presented
    
    Input Format
    The first line contains two space-separated integers, n, the size of arr to create, and q, the number of queries, respectively.
    Each of the q subsequent lines contains a query string, queries[i].
    
    Constraint
    - 1<= n,q <= 10^5
    - 0<= x,y <= 10^9
    - It is guaranteed that query type 2 will never query an empty array or index.

---
+++ DynamicArrayProblem.cs
@@ -0,0 +1,81 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'dynamicArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<int> dynamicArray(int n, List<List<int>> queries)
+    {
+        List<int> ans = new List<int>();
+        List<List<int>> res = new List<List<int>>();
+        int lastAnswer = 0;
+        for (int i = 0; i < n; i++)
+        {
+            res.Add(new List<int>());
+        }
+        for (int i = 0; i < queries.Count; i++)
+        {
+            if (queries[i][0] == 1)
+            {
+                int index = ((queries[i][1] ^ lastAnswer) % n);
+                res[index].Add(queries[i][2]);
+            }
+            else if (queries[i][0] == 2)
+            {
+                int index = ((queries[i][1] ^ lastAnswer) % n);
+                lastAnswer = res[index][queries[i][2] % (res[index].Count)];
+                ans.Add(lastAnswer);
+            }
+        }
+        return ans;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int q = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> queries = new List<List<int>>();
+
+        for (int i = 0; i < q; i++)
+        {
+            queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+        }
+
+        List<int> result = Result.dynamicArray(n, queries);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
