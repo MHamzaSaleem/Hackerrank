74acf687215401db8f69eb1af65fcb620264fde4
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-02-16 16:03:02 +0500

    2D Arrays
    Objective
    Today, we are building on our knowledge of arrays by adding another dimension. Check out the Tutorial tab for learning materials and an instructional video.
    
    Context
    Given a 6 x 6 2D Array, A:
    
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    
    We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
    
    a b c
      d
    e f g
    
    There are 16 hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
    
    Task
    Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
    
    Example
    In the array shown above, the maximum hourglass sum is 7 for the hourglass in the top left corner.
    
    Input Format
    There are 6 lines of input, where each line contains 6 space-separated integers that describe the 2D Array A.
    
    Constraints
     -9<=A[i][j]<=9
     0<=i,j<=5
    
    Output Format
    Print the maximum hourglass sum in A.
    
    Sample Input
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 2 4 4 0
    0 0 0 2 0 0
    0 0 1 2 4 0
    
    Sample Output
    19

---
+++ TwoDimensionalArrayHourGlassProblem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class TwoDimensionalArrayHourGlassProblem
+    {
+        public static void Main(string[] args)
+        {
+            List<List<int>> arr = new List<List<int>>()
+            {
+               new List<int> { 1, 1, 1, 0, 0, 0 },
+               new List<int> { 0, 1, 0, 0, 0, 0 },
+               new List<int> { 1, 1, 1, 0, 0, 0 },
+               new List<int> { 0, 0, 2, 4, 4, 0 },
+               new List<int> { 0, 0, 0, 2, 0, 0 },
+               new List<int> { 0, 0, 1, 2, 4, 0 }
+            }; 
+            int largestCount = int.MinValue;
+            if (arr.Count > 0)
+            {
+                for (int i = 0; i < arr.Count - 2; i++)
+                {
+                    for (int j = 0; j < arr[i].Count - 2; j++)
+                    {
+                        int temp = 0;
+                        for (int k = i; k < i + 3; k++)
+                        {
+                            for (int l = j; l < j + 3; l++)
+                            {
+                                if ((k == i + 1) && (l == j || l == j + 2))
+                                    temp += 0;
+                                else
+                                {
+                                    temp += arr[k][l];
+                                }
+                            }
+                        }
+                        if (temp > largestCount)
+                            largestCount = temp;
+                    }
+                }
+            }
+            Console.WriteLine(largestCount);
+        }
+    }
+}
