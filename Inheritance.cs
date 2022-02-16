031b75dc8b916e9fefc70f17c676cf4289af4e32
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-02-16 16:38:13 +0500

    Inheritance Problem
    Objective
    Today, we're delving into Inheritance. Check out the attached tutorial for learning materials and an instructional video.
    
    Task
    You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
    
    Complete the Student class by writing the following:
    
    A Student class constructor, which has 4 parameters:
    A string, firstName.
    A string, lastName.
    An integer, idNumber.
    An integer array (or vector) of test scores, scores.

---
+++ Inheritance.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+
+class Person
+{
+    protected string firstName;
+    protected string lastName;
+    protected int id;
+
+    public Person() { }
+    public Person(string firstName, string lastName, int identification)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = identification;
+    }
+    public void printPerson()
+    {
+        Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+    }
+}
+
+class Student : Person
+{
+    private int[] testScores;
+    // public string firstName;
+    // public string lastName;
+    // public int id;
+    /*	
+    *   Class Constructor
+    *   
+    *   Parameters: 
+    *   firstName - A string denoting the Person's first name.
+    *   lastName - A string denoting the Person's last name.
+    *   id - An integer denoting the Person's ID number.
+    *   scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+    public Student(string firstname, string lastname, int id, int[] scores)
+    {
+        this.firstName = firstname;
+        this.lastName = lastname;
+        this.id = id;
+        this.testScores = scores;
+    }
+    /*	
+    *   Method Name: Calculate
+    *   Return: A character denoting the grade.
+    */
+    // Write your method here
+    public char Calculate()
+    {
+        int temp = 0;
+        for (int i = 0; i < this.testScores.Length; i++)
+        {
+            temp += this.testScores[i];
+        }
+        double percentageMark = temp / this.testScores.Length;
+        if (percentageMark >= 90 && percentageMark <= 100)
+            return 'O';
+        else if (percentageMark >= 80 && percentageMark < 90)
+            return 'E';
+        else if (percentageMark >= 70 && percentageMark < 80)
+            return 'A';
+        else if (percentageMark >= 55 && percentageMark < 70)
+            return 'P';
+        else if (percentageMark >= 40 && percentageMark < 55)
+            return 'D';
+        else
+            return 'T';
+    }
+}
+
+class Solution
+{
+    static void Main()
+    {
+        string[] inputs = Console.ReadLine().Split();
+        string firstName = inputs[0];
+        string lastName = inputs[1];
+        int id = Convert.ToInt32(inputs[2]);
+        int numScores = Convert.ToInt32(Console.ReadLine());
+        inputs = Console.ReadLine().Split();
+        int[] scores = new int[numScores];
+        for (int i = 0; i < numScores; i++)
+        {
+            scores[i] = Convert.ToInt32(inputs[i]);
+        }
+
+        Student s = new Student(firstName, lastName, id, scores);
+        s.printPerson();
+        Console.WriteLine("Grade: " + s.Calculate() + "\n");
+    }
+}
