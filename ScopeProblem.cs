1eefdd0b9ddc0c23b5f4296811df88d7c0e47473
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-02-17 10:12:44 +0500

    Scope Problem
    Objective
    Today we're discussing scope. Check out the Tutorial tab for learning materials and an instructional video!
    
    The absolute difference between two integers, a and b, is written as |a-b|. The maximum absolute difference between two integers in a set of positive integers, elements, is the largest absolute difference between any two integers in _elements.
    
    The Difference class is started for you in the editor. It has a private integer array (elements) for storing N non-negative integers, and a public integer (maximumDifference) for storing the maximum absolute difference.
    
    Task
    Complete the Difference class by writing the following:
    
    A class constructor that takes an array of integers as a parameter and saves it to the  instance variable.
    A computeDifference method that finds the maximum absolute difference between any  numbers in  and stores it in the maximumDifference instance variable.
    
    Input Format
    You are not responsible for reading any input from stdin. The locked Solution class in the editor reads in 2 lines of input. The first line contains N, the size of the elements array. The second line has  space-separated integers that describe the _elements array.
    
    Constraints
    1<=N<=10
    1<=_elements[i]<=100, where 0<=i<=N-1
    
    Output Format
    You are not responsible for printing any output; the Solution class will print the value of the maximumDifference instance variable.
    
    Sample Input
    STDIN   Function
    -----   --------
    3       __elements[] size N = 3
    1 2 5   __elements = [1, 2, 5]
    
    Sample Output
    4

---
+++ ScopeProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    public Difference(int[] arr)
+    {
+        this.elements = arr;
+    }
+
+    public void computeDifference()
+    {
+        for (int i = 0; i < this.elements.Length; i++)
+        {
+            for (int j = i + 1; j < this.elements.Length; j++)
+            {
+                int temp = this.elements[i] - this.elements[j];
+                if (Math.Abs(temp) > this.maximumDifference)
+                    this.maximumDifference = Math.Abs(temp);
+            }
+        }
+    }
+
+} // End of Difference Class
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+    }
+}
